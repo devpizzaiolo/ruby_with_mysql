a869acdc33529f95c2adb35068c270a41a58c987
aakash.dynamic.methods@gmail.com <aakash.dynamic.methods@gmail.com>
2022-05-02 16:45:04 +0530

    - First Commit

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,17 @@
+class RegistrationsController < ApplicationController
+    def new
+        @user = User.new
+    end
+    def create
+        @user= User.new(user_params)
+        if @user.save
+            render json: "Created"
+        else
+            render :new
+        end
+    end
+    private
+    def user_params
+        params.require(:user).permit(:name,:password)
+    end
+end
